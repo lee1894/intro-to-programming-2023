ef60b530b48d49120ff52067f287a9c82ae0ba25
Emily Haag <emilyhaag1894@gmail.com>
2023-11-26 19:13:27 -0800

    adding message log and remove button

--- js/index.js
+++ js/index.js
@@ -18,3 +18,33 @@ for (let i = 0; i < skills.length; i++) {
     skillsList.appendChild(skill);
 };
 
+let messageForm = document.getElementsByName('leave_message')[0];
+messageForm.addEventListener('submit', event => {
+    event.preventDefault();
+    let userName= event.target.usersName.value; 
+    let email = event.target.usersEmail.value;
+    let message = event.target.usersMessage.value;
+   
+    let messageSection =  document.querySelector("#messages");
+   
+    let newMessage= document.createElement("li");
+   
+    let blah  = "<a href=\'mailto:" + email + "\'>" + userName + "</a> Wrote: " + message + " ";
+    console.log(blah)
+    newMessage.innerHTML = blah;
+    messageSection.appendChild(newMessage);
+    
+    
+    let removeButton= document.createElement("button");
+    removeButton.innerHTML = 'Remove';
+    newMessage.appendChild(removeButton);
+    let button1 = () => {
+        let entry = removeButton.parentNode;
+        entry.remove();
+    }
+  
+    removeButton.addEventListener('click', button1);
+    
+
+    event.target.reset();
+  });
